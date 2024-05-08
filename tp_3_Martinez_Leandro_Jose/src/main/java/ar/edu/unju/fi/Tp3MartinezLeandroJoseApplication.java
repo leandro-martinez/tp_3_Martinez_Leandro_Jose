30dcf4a13a40ce1578199e551c8aa43940cfd8e9
leandro-martinez <leandromartinez022704@gmail.com>
2024-05-08 15:42:47 -0300

    creacion del proyecto

---
+++ tp_3_Martinez_Leandro_Jose/src/main/java/ar/edu/unju/fi/Tp3MartinezLeandroJoseApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3MartinezLeandroJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3MartinezLeandroJoseApplication.class, args);
+	}
+
+}
